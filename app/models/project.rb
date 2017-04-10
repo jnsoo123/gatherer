5f87ced75d98e501fa7c5408d0249892d3c3e257
John Neil Soo <neil.soo@adish.co.jp>
2017-04-10 11:40:34 +0800

    refactored Project model and checked if specs still passed

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,11 +5,15 @@ class Project < ApplicationRecord
     tasks.reject(&:complete?).empty?
   end
 
+  def incomplete_tasks
+    tasks.reject(&:complete?)
+  end
+
   def total_size
     tasks.sum(&:size)
   end
 
   def remaining_size
-    tasks.reject(&:complete?).sum(&:size)
+    incomplete_tasks.sum(&:size)
   end
 end
