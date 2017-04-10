7a3397b2323a36251cc17a46768bc014f4d66f15
John Neil Soo <neil.soo@adish.co.jp>
2017-04-10 10:20:00 +0800

    refactored project specs

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -6,15 +6,12 @@ RSpec.describe Project do
     let (:task)    { Task.new }
 
     it "considers a project with no tasks to be done" do
-      project = Project.new
-      expect(project.done?).to be_truthy
+      expect(project).to be_done
     end
 
     it "knows a project with a task is not done" do
-      project = Project.new
-      task = Task.new
       project.tasks << task
-      expect(project.done?).to be_falsy
+      expect(project).to_not be_done
     end
   end
 end
