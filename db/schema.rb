92626900ef2ffe1d9aec5753a08801478345034b
John Neil Soo <neil.soo@adish.co.jp>
2017-04-10 13:51:58 +0800

    added name attribute to Project. added title attribute to Task. added view specs for the code to pass

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410045852) do
+ActiveRecord::Schema.define(version: 20170410054926) do
 
   create_table "projects", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "due_date"
+    t.string   "name"
   end
 
   create_table "tasks", force: :cascade do |t|
@@ -24,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170410045852) do
     t.datetime "updated_at",   null: false
     t.integer  "size"
     t.datetime "completed_at"
+    t.string   "title"
     t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
