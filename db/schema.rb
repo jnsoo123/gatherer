5cf58b6ad46abdffbd85158cc6b4bb2e581ec23f
John Neil Soo <neil.soo@adish.co.jp>
2017-04-10 09:50:15 +0800

    added Task model to belong to Project

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410013157) do
+ActiveRecord::Schema.define(version: 20170410014722) do
 
   create_table "projects", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer  "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
 end
