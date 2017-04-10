cdc13ebb740072e510f6f1e750874548522de5e8
John Neil Soo <neil.soo@adish.co.jp>
2017-04-10 11:08:06 +0800

    made code pass from new specs from last commit

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,12 @@ class Project < ApplicationRecord
   def done?
     tasks.reject(&:complete?).empty?
   end
+
+  def total_size
+    tasks.sum(&:size)
+  end
+
+  def remaining_size
+    tasks.reject(&:complete?).sum(&:size)
+  end
 end
