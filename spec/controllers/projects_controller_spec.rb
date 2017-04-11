a46da63684e615888157df428426471be4907a39
John Neil Soo <neil.soo@adish.co.jp>
2017-04-11 15:00:12 +0800

    refactored specs

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -8,6 +8,16 @@ RSpec.describe ProjectsController, type: :controller do
       expect(assigns(:action).project.name).to eq "Runway"
     end
 
+    it "creates a project (mock version)" do
+      fake_action = instance_double(CreatesProject, create: true)
+      expect(CreatesProject).to receive(:new)
+        .with(name: "Runway", task_string: "start something:2")
+        .and_return(fake_action)
+      post :create, params: { project: { name: "Runway", tasks: "start something:2" } }
+      expect(response).to redirect_to projects_path
+      expect(assigns(:action)).to_not be_nil
+    end
+
     it "goes back to form on failure" do
       post :create, params: { project: { name: "", tasks: "" } }
       expect(response).to render_template :new
